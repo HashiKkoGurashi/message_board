e554162b5ec5cfc87ab97d624fc37fe3392740d9
kouhei.hashimoto <k.hashimoto011235@icloud.com>
2022-08-12 17:05:53 +0900

    Hibernate設定完了

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -16,6 +16,9 @@ public class IndexServlet extends HttpServlet {
         super();
     }
 
+
+
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().append("Served at: ").append(request.getContextPath());
     }
